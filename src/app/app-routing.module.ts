e77093d5a1adc1dde5a8cc32ed053640c038fd26
HenrykMichalicki <henrykmandv@gmail.com>
2021-08-26 11:12:34 -0300

    criação do componente cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'entrar', component: EntrarComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
