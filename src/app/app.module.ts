f471f26848540ebc520ab21039fe15545de3830a
Amanda Corrêa <84135805+CorreaCoding@users.noreply.github.com>
2021-08-26 10:00:06 -0300

    Adicionando componente entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
