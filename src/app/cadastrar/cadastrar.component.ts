477d153ffb1c260806b1f116d7c54ffa1b9d354d
Juan Barros <juanhlealbarros@gmail.com>
2021-08-26 17:04:49 -0300

    validation quase pronto

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
+  styleUrls: ['./cadastrar.component.css'],
 })
 export class CadastrarComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.validatePreenchido();
   }
 
+  validatePreenchido() {
+    let usuario = <HTMLInputElement>document.getElementById('email');
+    if (usuario?.value != '') {
+      usuario.classList.add('preenchido');
+    } else {
+      usuario.classList.remove('preenchido');
+    }
+  }
 }
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-entrar',
   templateUrl: './entrar.component.html',
-  styleUrls: ['./entrar.component.css']
+  styleUrls: ['./entrar.component.css'],
 })
 export class EntrarComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.validatePreenchido();
   }
 
+  validatePreenchido() {
+    let usuario = <HTMLInputElement>document.getElementById('usuario');
+    if (usuario?.value != '') {
+      usuario.classList.add('preenchido');
+    } else {
+      usuario.classList.remove('preenchido');
+    }
+  }
 }
