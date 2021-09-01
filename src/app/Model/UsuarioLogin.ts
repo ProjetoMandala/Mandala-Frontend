84015f448e6f5dd7b2a623292ff16111e933837a
Juan Barros <juanhlealbarros@gmail.com>
2021-09-01 15:12:16 -0300

    hfdj

--- src/app/Model/UsuarioLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UsuarioLogin {
-    public id: number;
-    public imagem_perfil: string;
-    public nome: string;
-    public senha: string;
-    public token: string;
-    public usuario: string;
-    
-}
