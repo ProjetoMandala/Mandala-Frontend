21f7cd35b7b110741363762fc524ba14b50125a9
Juan Barros <juanhlealbarros@gmail.com>
2021-09-01 14:41:03 -0300

    coisas

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -5,20 +5,15 @@ import { environment } from 'src/environments/environment.prod';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
-
-  constructor(
-    private router: Router
-  ) { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-
-    if(environment.token == ''){
-      alert('Sua seção expirou, faça o login novamente')
-      this.router.navigate(['/entrar'])
+    if (environment.token == '') {
+      alert('Sua seção expirou, faça o login novamente');
+      this.router.navigate(['/entrar']);
     }
   }
-
 }
