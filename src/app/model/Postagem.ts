142e9a59a8cb5a7e9426649e16e4375252b0359c
Juan Barros <juanhlealbarros@gmail.com>
2021-09-01 15:16:35 -0300

    sla

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,13 @@
+import { Tema } from './Tema';
+import { Usuario } from './Usuario';
+
+export class Postagem {
+  public curtida: number;
+  public id: number;
+  public imagem: string;
+  public texto: string;
+  public titulo: string;
+  public usuario: Usuario;
+  public video: string;
+  public tema: Tema;
+}
---
+++ src/app/model/UsuarioLogin.ts
@@ -0,0 +1,8 @@
+export class UsuarioLogin {
+  public id: number;
+  public imagem_perfil: string;
+  public nome: string;
+  public senha: string;
+  public token: string;
+  public usuario: string;
+}
